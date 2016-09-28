ec5a1eb91165867010d78320c4c9335ca362cac9
vnramachandra <ramachandra.naragund@robosoftin.com>
2016-09-28 16:01:34 +0530

    Modified for v1.0.0

---
+++ MyFramework.framework/Headers/MyFramework.h
@@ -0,0 +1,20 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Ramachandra Naragund on 26/09/16.
+//  Copyright © 2016 Robosoft Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MyMainClass.h"
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
---
+++ MyFramework.framework/Headers/MyMainClass.h
@@ -0,0 +1,15 @@
+//
+//  MyMainClass.h
+//  MyFramework
+//
+//  Created by Ramachandra Naragund on 26/09/16.
+//  Copyright © 2016 Robosoft Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyMainClass : NSObject
+
++(void) greetings;
+
+@end
